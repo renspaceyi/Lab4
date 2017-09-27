e76fb045920da9f7e183c8ab9daf6bad935322f2
User <User@DESKTOP-95M0R90>
2017-09-27 14:05:46 -0500

    Commit !

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,43 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * Contains a function that tells you the longest shared substring between two strings.
+ *
+ */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+public class ReclamationProject {
+    /** Takes in two strings and returns the longest shared element between the two.
+     *  @param string1 holds the first string
+     *  @param string2 holds the second string
+     *
+     *  @return returns the longest shared element between the two strings
+     *
+     * */
+    static String doit(final String string1, final String string2) {
+        String shorterString; // a
+        String longerString; // b
+        if (string1.length() > string2.length()) {
+            shorterString = string2;
+            longerString = string1;
+        } else {
+            longerString = string2;
+            shorterString = string1;
+        }
+        String r = "";
+
+        for (int i = 0; i < shorterString.length(); i++) {
+            for (int j = shorterString.length() - i; j > 0; j--) {
+                for (int k = 0; k < longerString.length() - j; k++) {
+                    if (shorterString.regionMatches(i, longerString, k, j) && j > r.length()) {
+                        r = shorterString.substring(i, i + j);
+                    }
+                }
+            } // Ah yeah
+        }
+        return r;
+
+    }
+
+
 }
